6c96b37c13940fe8fc456931289017e0c1fb6c3e
dbojdo <daniel.bojdo@web-it.eu>
2012-12-08 16:09:54 +0100

    [new] phpcr tools

---
+++ Document/Generic.php
@@ -0,0 +1,27 @@
+<?php
+namespace Webit\Bundle\PHPCRToolsBundle\Document;
+
+use Webit\Tools\Text\Slugifier;
+use PHPCR\Util\UUIDHelper;
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+use Doctrine\ODM\PHPCR\Document\Generic as BaseGeneric;
+
+/**
+ * @author dbojdo
+ * @PHPCRODM\Document()
+ */
+class Generic extends BaseGeneric {
+	/**
+	 * 
+	 * @param string $name
+	 */
+	public function setNodename($name) {
+		$name = Slugifier::slugify($name);
+		if(empty($name)) {
+			$name = UUIDHelper::generateUUID();
+		};
+		
+		parent::setNodename($name);
+	}
+}
+?>
---
+++ Document/ReferenceableGeneric.php
@@ -0,0 +1,30 @@
+<?php
+namespace Webit\Bundle\PHPCRToolsBundle\Document;
+
+use Doctrine\ORM\Id\UuidGenerator;
+
+use Webit\Tools\Text\Slugifier;
+use PHPCR\Util\UUIDHelper;
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+
+/**
+ * @author dbojdo
+ * @PHPCRODM\Document(referenceable=true)
+ */
+class ReferenceableGeneric extends Generic {
+	/**
+	 * @var string
+	 * @PHPCRODM\Uuid
+	 */
+	protected $uuid;
+	
+	
+	public function __construct() {
+		$this->uuid = UUIDHelper::generateUUID();
+	}
+	
+	public function getUuid() {
+		return $this->uuid;
+	}
+}
+?>
